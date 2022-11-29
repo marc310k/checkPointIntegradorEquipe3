1499a79cd52668385730b83ac5a8c12d496fddbe
Marcelo Pacholak <89089256+marc310k@users.noreply.github.com>
2022-11-28 22:21:23 -0300

    Atualizaçao Service

--- src/main/java/com/dhcleanodonto/cleanodonto/service/LoginService.java
+++ src/main/java/com/dhcleanodonto/cleanodonto/service/LoginService.java
@@ -1,70 +1,72 @@
 package com.dh.cleanodonto.cleanodonto.service;
 
-import com.dh.cleanodonto.cleanodonto.dto.LoginDTO;
-import com.dh.cleanodonto.cleanodonto.model.Login;
-import com.dh.cleanodonto.cleanodonto.repository.LoginRespository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import com.dh.cleanodonto.cleanodonto.dto.LoginDTO;
+import com.dh.cleanodonto.cleanodonto.model.Login;
+import com.dh.cleanodonto.cleanodonto.repository.LoginRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
-    //INJEÇÃO DE DEPENDENCIA
-    @Autowired
-    private LoginRespository loginRepository;
+	
+	 @Autowired
+	    private LoginRepository  loginRepository;
 
-    //METODO LISTAR TUDO
-    public List<LoginDTO> getAll(){
+	    //METODO LISTAR TUDO
+	    public List<LoginDTO> getAll(){
 
-        List<Login> lista = loginRepository.findAll();
-        List<LoginDTO> listaDTO = new ArrayList<>();
-        for (Login login : lista) {
-            listaDTO.add(login.toDTO()); //CONVERSOR ARRAY PARA STRING
-        }
+	        List<Login> lista = loginRepository.findAll();
+	        List<LoginDTO> listaDTO = new ArrayList<>();
+	        for (Login login : lista) {
+	            listaDTO.add(login.toDTO()); //CONVERSOR ARRAY PARA STRING
+	        }
 
-        return listaDTO;
+	        return listaDTO;
 
-    }
+	    }
 
-    public LoginDTO getOne(int id) {
-        Optional<Login> optional = loginRepository.findById(id);
-        Login login = optional.orElse( new Login());
-        return login.toDTO();
-    }
-
-
-    public LoginDTO save(Login login) {
-        return loginRepository.save(login).toDTO();
-
-    }
+	    public LoginDTO getOne(int id) {
+	        Optional<Login> optional = loginRepository.findById(id);
+	        Login login = optional.orElse( new Login());
+	        return login.toDTO();
+	    }
 
 
-    public LoginDTO update(int id, Login novoLogin) {
+	    public LoginDTO save(Login login) {
+	        return loginRepository.save(login).toDTO();
 
-        //VERIFICAR SE O REGISTRO EXISTE
-        Optional<Login> optional = loginRepository.findById(id);
-
-        //SE EXISTIR
-        if(optional.isPresent() == true) {
-            //PEGAR O OBJETO DENTRO O OPTIONAL VINDO DO BANCO E SETAR COM O VALOR DE ENTRADA DO PARAMETRO DA METODO
-            Login loginBD = optional.get();
-            loginBD.setLogin(novoLogin.getLogin());
-            loginBD.setSenha(novoLogin.getSenha());
-            loginBD.setAcesso(novoLogin.getAcesso());
+	    }
 
 
-            return loginRepository.save(loginBD).toDTO();
-        }
+	    public LoginDTO update(int id, Login novoLogin) {
 
-        else {
-            return new Login().toDTO();
-        }
-    }
+	        //VERIFICAR SE O REGISTRO EXISTE
+	        Optional<Login> optional = loginRepository.findById(id);
 
-    public void delete(int id) {
-        loginRepository.deleteById(id);
-    }
+	        //SE EXISTIR
+	        if(optional.isPresent() == true) {
+	            //PEGAR O OBJETO DENTRO O OPTIONAL VINDO DO BANCO E SETAR COM O VALOR DE ENTRADA DO PARAMETRO DA METODO
+	            Login loginBD = optional.get();
+	            loginBD.setLogin(novoLogin.getLogin());
+	            loginBD.setSenha(novoLogin.getSenha());
+	            loginBD.setAcesso(novoLogin.getAcesso());
+
+
+	            return loginRepository.save(loginBD).toDTO();
+	        }
+
+	        else {
+	            return new Login().toDTO();
+	        }
+	    }
+
+	    public void delete(int id) {
+	        loginRepository.deleteById(id);
+	    }
+
+
 }

96b98e8dae9486ed9b81488ec51c21fa59285c12
Adriana Guedes <88890733+Adriana-Guedes@users.noreply.github.com>
2022-12-11 19:17:26 -0300

    Add files via upload

---
+++ src/test/java/com/dhcleanodonto/cleanodonto/test/java/com/dh/cleanodonto/cleanodonto/CleanodontoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.dh.cleanodonto.cleanodonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CleanodontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

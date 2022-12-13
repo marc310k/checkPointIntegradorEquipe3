978d4836a2910cb73d8f5046983925d1329e69e2
Adriana Guedes <88890733+Adriana-Guedes@users.noreply.github.com>
2022-12-13 00:25:57 -0300

    Delete src directory

--- src/main/java/com/dhcleanodonto/cleanodonto/repository/LoginRespository.java
+++
@@ -1,9 +0,0 @@
-package com.dh.cleanodonto.cleanodonto.repository;
-
-import com.dh.cleanodonto.cleanodonto.model.Login;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LoginRespository extends JpaRepository<Login, Integer> {
-}
